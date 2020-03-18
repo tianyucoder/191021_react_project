c1b7a1c02349201ecf08061743bc1aec2b91a1dc
tianyu5177 <zhangtianyu5177@sina.com>
2020-03-18 11:01:13 +0800

    更新

--- src/containers/left_nav/left_nav.jsx
+++ src/containers/left_nav/left_nav.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
 import {Menu} from 'antd';
-import {
-  AppstoreOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
-} from '@ant-design/icons';
+import menus from '../../config/menu_config'
 import logo from '../../static/imgs/logo.png'
 import './css/left_nav.less'
-const {SubMenu} = Menu;
+const {SubMenu,Item} = Menu;
 
 export default class LeftNav extends Component {
+
+	createMenu = (menuArr)=>{
+		return menuArr.map((menuObj)=>{
+			if(!menuObj.children){
+				return (
+					<Item key={menuObj.key}>
+						<menuObj.icon/>
+						<span>{menuObj.title}</span>
+					</Item>
+				)
+			}else{
+				return (
+					<SubMenu
+						key={menuObj.key}
+						title={
+							<span>
+								<menuObj.icon/>
+								<span>{menuObj.title}</span>
+							</span>
+						}
+					>
+						{this.createMenu(menuObj.children)}
+					</SubMenu>
+				)
+			}
+		})
+	}
 
 	render() {
 		return (
@@ -22,53 +43,12 @@ export default class LeftNav extends Component {
 				</div>
 				<div>
 					<Menu
-						defaultSelectedKeys={['1']}
-						defaultOpenKeys={['sub1']}
-						mode="inline"
-						theme="dark"
+						defaultSelectedKeys={['home']} //一上来就选中谁
+						defaultOpenKeys={['sub1000']}//默认展开哪个菜单
+						mode="inline"//内嵌模式
+						theme="dark"//暗色主题
 					>
-						<Menu.Item key="1">
-							<PieChartOutlined />
-							<span>Option 1</span>
-						</Menu.Item>
-						<Menu.Item key="2">
-							<DesktopOutlined />
-							<span>Option 2</span>
-						</Menu.Item>
-						<Menu.Item key="3">
-							<ContainerOutlined />
-							<span>Option 3</span>
-						</Menu.Item>
-						<SubMenu
-							key="sub1"
-							title={
-								<span>
-									<MailOutlined />
-									<span>Navigation One</span>
-								</span>
-							}
-						>
-							<Menu.Item key="5">Option 5</Menu.Item>
-							<Menu.Item key="6">Option 6</Menu.Item>
-							<Menu.Item key="7">Option 7</Menu.Item>
-							<Menu.Item key="8">Option 8</Menu.Item>
-						</SubMenu>
-						<SubMenu
-							key="sub2"
-							title={
-								<span>
-									<AppstoreOutlined />
-									<span>Navigation Two</span>
-								</span>
-							}
-						>
-							<Menu.Item key="9">Option 9</Menu.Item>
-							<Menu.Item key="10">Option 10</Menu.Item>
-							<SubMenu key="sub3" title="Submenu">
-								<Menu.Item key="11">Option 11</Menu.Item>
-								<Menu.Item key="12">Option 12</Menu.Item>
-							</SubMenu>
-						</SubMenu>
+						{this.createMenu(menus)}
 					</Menu>
 				</div>
 			</div>
