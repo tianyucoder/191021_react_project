dccb5a06815a2b9743b0ea3c782b6009456f31f9
tianyu5177 <zhangtianyu5177@sina.com>
2020-03-20 09:54:18 +0800

    更新

--- src/containers/category/category.jsx
+++ src/containers/category/category.jsx
@@ -1,19 +1,50 @@
-import React, { Component } from 'react'
-import {reqCategoryList} from '../../ajax'
+import React,{Component} from 'react'
+import {Card,Button,Table} from 'antd';
+import {PlusCircleOutlined} from '@ant-design/icons';
+
 
 export default class Category extends Component {
 
-	demo = async()=>{
-		let result = await reqCategoryList()
-		console.log(result);
-	}
-
 	render() {
+		const dataSource = [
+			{
+				key: '1',
+				name: '测试分类一',
+			},
+			{
+				key: '2',
+				name: '测试分类二',
+			},
+			{
+				key: '3',
+				name: '测试分类三',
+			},
+		];
+
+		//columns是配置Table列的，是一个相当重要的配置项
+		const columns = [
+			{
+				title: '分类名', //列名
+				dataIndex: 'name', //该列要展示什么信息
+				key: 'name',
+			},
+			{
+				title: '操作',
+				//dataIndex: 'caozuo',
+				key: 'ft789u0iiuyg8opk',
+				align:'center',
+				width:'15%',
+				render:()=><Button type="link" href="">修改分类</Button>
+			},
+		];
 		return (
-			<div>
-				Category分类管理
-				<button onClick={this.demo}>点我获取商品分类数据</button>
-			</div>
+			<Card extra={<Button type="primary"><PlusCircleOutlined/>添加</Button>}>
+				<Table 
+					bordered
+					dataSource={dataSource} 
+					columns={columns} 
+				/>
+			</Card>
 		)
 	}
 }
